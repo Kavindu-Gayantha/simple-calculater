b7a9170ecd24df4426273c0f0865e0c200c2d419
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-05-05 20:07:06 +0530

    complete the cal with exit button and made jar file

--- JAVACal/src/calculaterV1/frameCalculater.java
+++ JAVACal/src/calculaterV1/frameCalculater.java
@@ -597,7 +597,8 @@ public class frameCalculater extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        System.exit(0);
+//        System.exit(0);
+        dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
     
     /**
